1d0ee4f61c121136caf942be0d13a497d2c0cc5a
Saad Elbeleidy <sbeleidy90@gmail.com>
2018-11-29 03:13:58 -0700

    Select model

--- hp_search.py
+++ hp_search.py
@@ -2,12 +2,15 @@
 """
 
 from sklearn.datasets import load_iris
+from sklearn.svm import SVC
 
 # Step 1: Prepare data
 iris = load_iris()
 # This data is a classification dataset with 3 classes and an input of 4 features
 
 # Step 2: Select Model
+svc = SVC(random_state=0)
+# We can set a particular hyperparameter for all tests by adding it to the original instantiation
 
 # Step 3: Determine Hyperparameters to test
 
