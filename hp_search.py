1ce00bd1c88f1e1eef6ae341a7a92c4eec4fe373
Saad Elbeleidy <sbeleidy90@gmail.com>
2018-11-29 03:12:27 -0700

    Add hp_search pseudocode

--- hp_search.py
+++ hp_search.py
@@ -1,2 +1,12 @@
 """A tutorial on hyperparameter search using GridSearchCV
 """
+
+# Step 1: Prepare data
+
+# Step 2: Select Model
+
+# Step 3: Determine Hyperparameters to test
+
+# Step 4: Determine evaluation metric
+
+# Step 5: Run search and determine best hyperparameters
