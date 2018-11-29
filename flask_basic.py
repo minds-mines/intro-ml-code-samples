098fd0d92d8fcc31b382cc6156ecab2b8c49131a
Saad Elbeleidy <sbeleidy90@gmail.com>
2018-11-29 06:06:43 -0700

    add current working code

---
+++ flask_basic.py
@@ -0,0 +1,125 @@
+"""A simple example flask application
+"""
+from flask import Flask, jsonify, request, render_template
+import numpy as np
+from keras.models import load_model
+from keras.backend import clear_session
+import joblib
+import cv2
+import traceback
+
+clear_session()
+app = Flask(__name__)
+iris_model = None
+mnist_model = None
+
+
+def load_iris_model():
+    global iris_model
+    # Step 1: Determine file location
+    model_file = 'models/sk-model.joblib'
+    # Step 2: Load model
+    iris_model = joblib.load(model_file)
+
+def load_mnist_model():
+    global mnist_model
+    model_file = 'models/keras-mnist.h5'
+    mnist_model = load_model(model_file)
+    mnist_model._make_predict_function()
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/json")
+def json_endpoint():
+    return jsonify({
+        "message": "Hello World!"
+    })
+
+
+@app.route("/variables/<variable>")
+def example_variable(variable):
+    return jsonify({
+        "message": f"The variable you entered is {variable}"
+    })
+
+@app.route("/request-args")
+def example_request_args():
+    try:
+        a = request.args["a"]
+        b = request.args["b"]
+        c = request.args["c"]
+        return jsonify({
+            "message": f"You entered a = {a}, b= {b} and c= {c}."
+        })
+    except:
+        return jsonify({
+            "message": f"You did not provide one of a, b, or c."
+        })
+
+@app.route("/iris")
+def predict_iris():
+    # To predict the iris species we need:
+    # sepal length in cm - sepal_length
+    # sepal width in cm - sepal_width
+    # petal length in cm - petal_length
+    # petal width in cm - petal_width
+    try:
+        sl = request.args["sepal_length"]
+        sw = request.args["sepal_width"]
+        pl = request.args["petal_length"]
+        pw = request.args["petal_width"]
+    except:
+        return jsonify({
+            "message": f"You did not provide one of sepal_length, sepal_width, petal_length, or petal_width."
+        })
+    vector = np.array([sl, sw, pl, pw])
+    pred = iris_model.predict([vector])
+    classes = ["Setosa","Versicolour","Virginica"]
+    species = classes[pred[0]]
+    return jsonify({
+        "message": f"The predicted species for the observations you entered is {species} ",
+        "species": species
+    })
+
+
+@app.route("/iris-ui")
+def iris_ui():
+    return render_template("iris.html")
+    
+
+@app.route("/mnist", methods=["POST"])
+def mnist_predict():
+    try:
+        image =  request.files['file'].read()
+        print("Got image")
+        # https://stackoverflow.com/a/27537664/818687
+        arr = cv2.imdecode(np.fromstring(image, np.uint8), cv2.IMREAD_UNCHANGED)
+        print("CV2 read image")
+        my_image = arr / 255.0
+        my_images = my_image.reshape(1, 28, 28, 1)
+        print("Got here")
+
+        pred = mnist_model.predict(my_images)
+        n = int(np.argmax(pred))
+        print(n)
+        return jsonify({
+            "message": f"The predicted number for the uploaded image is {n} ",
+            "number": n
+        })
+    except Exception as e:
+        print(traceback.format_exc())
+        return jsonify({
+            "message": f"An error occurred. {e}"
+        })
+
+@app.route("/mnist-ui")
+def mnist_ui():
+    return render_template("mnist.html")
+
+
+if __name__ == '__main__':
+    load_iris_model()
+    load_mnist_model()
+    app.run(debug=True)
--- keras_save.py
+++ keras_save.py
@@ -2,31 +2,54 @@
 """
 
 from keras import Sequential
-from keras.datasets import boston_housing
-from keras.layers import Dense
+from keras.datasets import mnist
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.models import load_model
+from keras.utils import to_categorical
 
 # Load Dataset
-(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+img_rows, img_cols = 28, 28
+num_classes = 10 # 10 digits
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+print(x_train.shape)
+print(x_train.shape[1:])
+
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
 
 my_layers = [
-    Dense(64, input_shape=x_train.shape[1:], activation="relu"),
-    Dense(64, activation="relu"),
-    Dense(64, activation="relu"),
-    Dense(1) # We are predicting 1 target
+    Conv2D(32, kernel_size=(3,3), activation="relu", input_shape=input_shape),
+    Conv2D(64, kernel_size=(3,3), activation="relu"),
+    MaxPooling2D(pool_size=(2,2)),
+    Dropout(0.2),
+    Flatten(),
+    Dense(128, activation="relu"),
+    Dropout(0.2),
+    Dense(num_classes, activation="softmax") # Number of classes as output units
 ]
 
 model = Sequential(my_layers)
 
-model.compile(loss='mse',optimizer="adam", metrics=['mae'])
+model.compile(loss='categorical_crossentropy',optimizer="adam", metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=500, validation_split=0.2)
+model.fit(x_train, y_train, epochs=10, validation_split=0.2)
 
 scores = model.evaluate(x_test, y_test)
-print(f"Our model is able to predict with a mean squared error of {scores[0]:.2f} and a mean absolute error of {scores[1]:.2f}.")
+print(f"Our model is able to predict with an accuracy of {scores[1]:.2f}.")
 
-model.save("models/keras-boston.h5")
+model.save("models/keras-mnist.h5")
 
-loaded_model = load_model("models/keras-boston.h5")
+loaded_model = load_model("models/keras-mnist.h5")
 loaded_model_scores = loaded_model.evaluate(x_test, y_test)
-print(f"Our loaded model is able to predict with a mean squared error of {loaded_model_scores[0]:.2f} and a mean absolute error of {loaded_model_scores[1]:.2f}.")
+print(f"Our loaded model is able to predict with an accuracy of {loaded_model_scores[1]:.2f}.")
