7347118431dd945e39eee8959d51f5ce2a8864a3
Saad Elbeleidy <sbeleidy90@gmail.com>
2018-11-29 03:13:33 -0700

    Prep data

--- hp_search.py
+++ hp_search.py
@@ -1,7 +1,11 @@
 """A tutorial on hyperparameter search using GridSearchCV
 """
 
+from sklearn.datasets import load_iris
+
 # Step 1: Prepare data
+iris = load_iris()
+# This data is a classification dataset with 3 classes and an input of 4 features
 
 # Step 2: Select Model
 
